dd4814c338eb076dd9a3be8a2414cf83581b92cc
Danninon <danninon@gmail.com>
2023-07-17 18:34:25 +0300

    Added more embedding functions at elbow.py
    added streamlit_app_utils with the web utilities
    
    changed openAI to PR_SUMMARIZER
    added github into PR_SUMMARIZER
    
    basic functionality working

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import streamlit as st
 
-from utils import (
-    doc_loader, summary_prompt_creator, doc_to_final_summary,
+from PR_Summarizer import (
+    doc_loader, summary_prompt_creator, doc_to_final_summary, get_diff
 )
-from my_prompts import file_map, file_combine, youtube_map, youtube_combine
+from app_prompts import file_map, file_combine
 from streamlit_app_utils import check_gpt_4, check_key_validity, create_temp_file, create_chat_model, \
     token_limit, token_minimum
-
-from utils import transcript_loader
 
 
 def main():
@@ -17,80 +15,77 @@ def main():
 
     :return: None.
     """
-    st.title("Document Summarizer")
+    st.title("Probot - Pullrequest summarizer")
 
-    input_method = st.radio("Select input method", ('Upload a document', 'Enter a YouTube URL'))
+    # TODO: feature?
+    # input_method = st.radio("Select input method", ('Upload a document', 'Enter a YouTube URL'))
 
-    if input_method == 'Upload a document':
-        uploaded_file = st.file_uploader("Upload a document to summarize, 10k to 100k tokens works best!", type=['txt', 'pdf'])
+    open_ai_api_key = st.text_input("Enter Open-AI API key here, or contact the author if you don't have one.")
+    github_api_key = st.text_input("Enter Github API key here, or contact the author if you don't have one.")
+    repo_name = st.text_input("Enter Github repository name")
+    pr_number = st.text_input("Enter Pullrequest number")
 
-    if input_method == 'Enter a YouTube URL':
-        youtube_url = st.text_input("Enter a YouTube URL to summarize")
-
-    api_key = st.text_input("Enter API key here, or contact the author if you don't have one.")
-    st.markdown('[Author email](mailto:ethanujohnston@gmail.com)')
-    use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - progress bar will appear to get stuck as GPT-4 is slow)", value=True)
+    use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - "
+                            "progress bar will appear to get stuck as GPT-4 is slow)", value=True)
     find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
-    st.sidebar.markdown('# Made by: [Ethan](https://github.com/e-johnstonn)')
-    st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/e-johnstonn/docsummarizer)')
-    st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
-                        This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
+    st.sidebar.markdown('# Made by: ')
+    st.sidebar.markdown('Auther 1: [Bivas Dor](https://github.com/dorbivas)')
+    st.sidebar.markdown('Auther 2: [Inon Dan](https://github.com/danninon)')
 
+    st.sidebar.markdown('# Git link: [Probot](https://github.com/dorbivas/PRobot)')
+
+    st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your 
+    API key. This site is open source, so you can check the code yourself, or run the streamlit app 
+    locally.</small>""", unsafe_allow_html=True)
 
     if st.button('Summarize (click once and wait)'):
-        if input_method == 'Upload a document':
-            process_summarize_button(uploaded_file, api_key, use_gpt_4, find_clusters)
-
-        else:
-            doc = transcript_loader(youtube_url)
-            process_summarize_button(doc, api_key, use_gpt_4, find_clusters, file=False)
+        process_summarize_button(open_ai_api_key, github_api_key, repo_name, int(pr_number), use_gpt_4, find_clusters)
 
 
-def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_clusters, file=True):
+def process_summarize_button(open_ai_api_key, github_api_key, repo_name, pr_number, use_gpt_4, find_clusters):
     """
     Processes the summarize button, and displays the summary if input and doc size are valid
 
-    :param file_or_transcript: The file uploaded by the user or the transcript from the YouTube URL
 
-    :param api_key: The API key entered by the user
-
+    :param open_ai_api_key: Open-AI API key
+    :param github_api_key: GitHub API key
+    :param repo_name: GitHub repository's Name
+    :param pr_number: Repository's requested Pullrequest number
     :param use_gpt_4: Whether to use GPT-4 or not
 
     :param find_clusters: Whether to find optimal clusters or not, experimental
 
     :return: None
     """
-    if not validate_input(file_or_transcript, api_key, use_gpt_4):
+    if not validate_input(open_ai_api_key, use_gpt_4):
         return
 
     with st.spinner("Summarizing... please wait..."):
-        if file:
-            temp_file_path = create_temp_file(file_or_transcript)
-            doc = doc_loader(temp_file_path)
-            map_prompt = file_map
-            combine_prompt = file_combine
-        else:
-            doc = file_or_transcript
-            map_prompt = youtube_map
-            combine_prompt = youtube_combine
-        llm = create_chat_model(api_key, use_gpt_4)
+        git_diff, commit_message = get_diff(github_api_key, repo_name, pr_number)
+
+        temp_file_path = create_temp_file(git_diff)
+
+        doc = doc_loader(temp_file_path)
+        map_prompt = file_map
+        combine_prompt = file_combine
+
+        llm = create_chat_model(open_ai_api_key, use_gpt_4)
         initial_prompt_list = summary_prompt_creator(map_prompt, 'text', llm)
         final_prompt_list = summary_prompt_creator(combine_prompt, 'text', llm)
 
         if not validate_doc_size(doc):
-            if file:
-                os.unlink(temp_file_path)
+            os.unlink(temp_file_path)
             return
 
         if find_clusters:
-            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, api_key, use_gpt_4, find_clusters)
+            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, open_ai_api_key, use_gpt_4,
+                                           find_clusters)
 
         else:
-            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, api_key, use_gpt_4)
+            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, open_ai_api_key, use_gpt_4)
 
         st.markdown(summary, unsafe_allow_html=True)
-        if file:
-            os.unlink(temp_file_path)
+        os.unlink(temp_file_path)
 
 
 def validate_doc_size(doc):
@@ -112,7 +107,7 @@ def validate_doc_size(doc):
 
 
 def validate_input(api_key, use_gpt_4):
-   #TODO: validate other params (github api key, github repo, github number)
+    # TODO: validate other params (github api key, github repo, github number)
 
     if not check_key_validity(api_key):
         st.warning('Key not valid or API is down.')
@@ -127,4 +122,3 @@ def validate_input(api_key, use_gpt_4):
 
 if __name__ == '__main__':
     main()
-
